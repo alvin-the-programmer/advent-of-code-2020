ef689d480cb0b652a50cb8b801dd4ed5594c43f4
Alvin Zablan <azablan.dev@gmail.com>
2020-12-09 15:40:43 -0500

    add d9

---
+++ d9/part1.js
@@ -0,0 +1,38 @@
+const { read } = require('fs');
+
+const fs = require('fs').promises;
+
+const readLines = async () => {
+  const data = await fs.readFile('./input', {encoding: 'utf-8'});
+  return data.split('\n');
+};
+
+const solve = async () => {
+  const lines = await readLines();
+  const numbers = lines.map(Number);
+  return firstInvalid(numbers);
+};
+
+
+const firstInvalid = (numbers) => {
+  for (let i = 25; i < numbers.length; i++) {
+    const target = numbers[i];
+    const previousNums = numbers.slice(i - 25, i);
+    if (!hasSum(previousNums, target)) {
+     return target;
+    }
+  }
+};
+
+
+const hasSum = (numbers, target) => {
+  for (let numA of numbers) {
+    for (let numB of numbers) {
+      if (numA + numB === target)
+        return true;
+    }
+  }
+  return false;
+}
+
+solve().then(console.log);
