5c0b6a46ac0365ec4afcc2b852c0010452ab9cea
Alvin Zablan <azablan.dev@gmail.com>
2020-12-11 18:49:46 -0500

    d11 initial

--- d11/part2.js
+++ d11/part2.js
@@ -5,6 +5,27 @@ const readLines = async () => {
   return data.split('\n');
 };
 
+// const grid0 = [
+//   '.......#.',
+//   '...#.....',
+//   '.#.......',
+//   '.........',
+//   '..#L....#',
+//   '....#....',
+//   '.........',
+//   '#........',
+//   '...#.....'
+// ].map(s => s.split(''));
+
+// const grid1 = [
+//   [1,2,3,4,5,6],
+//   [7,8,9,10,11,12],
+//   [13,14,15,16,17,18],
+//   [19,20,21,22,23,24],
+//   [25,26,27,28,28,30],
+//   [31,32,33,34,35,36],
+// ];
+
 const solve = async () => {
   const lines = await readLines();
   let borderedGrid = lines.map(line => ['.', ...line.split(''), '.']);
@@ -37,8 +58,8 @@ const charCount = (grid, char) => {
 
 const getX = (grid, row, col) => {
   return [ 
-    ...grid[row].slice(0, col).reverse(),
-    ...grid[row].slice(col + 1)
+    grid[row].slice(0, col).reverse(),
+    grid[row].slice(col + 1)
   ];
 };
 
@@ -47,7 +68,7 @@ const getY = (grid, row, col) => {
   for (let i = row - 1; i >= 0; i -= 1)
     aboveAxis.push(grid[i][col]);
   
-  const belowAxis = []
+  const belowAxis = [];
   for (let i = row + 1; i < grid.length; i += 1)
     belowAxis.push(grid[i][col]);
 
@@ -58,16 +79,56 @@ const getY = (grid, row, col) => {
 };
 
 const getZ = (grid, row, col) => {
-  for (let i = 0; i < grid.length; i += 1) {
+  const leftAxis = [];
+  for (let i = 1; row - i >= 0 && col - i >= 0; i += 1)
+    leftAxis.push(grid[row - i][col - i]);
 
-  }
-  let i = row;
-  let j = col;
-
+  const rightAxis = [];
+  for (let i = 1; row + i < grid.length && col + i < grid[0].length; i += 1)
+    rightAxis.push(grid[row + i][col + i]);
+  
+  return [
+    leftAxis,
+    rightAxis
+  ];
 };
 
 const getW = (grid, row, col) => {
+  const leftAxis = [];
+  for (let i = 1; row + i < grid.length && col - i >= 0; i += 1)
+    leftAxis.push(grid[row + i][col - i]);
 
+  const rightAxis = [];
+    for (let i = 1; row - i >= 0 && col + i < grid[0].length; i += 1)
+      rightAxis.push(grid[row - i][col + i]);
+  
+  return [
+    leftAxis,
+    rightAxis
+  ];
+};
+
+const nearestNeighbors = (grid, row, col) => {
+  const neighbors = [];
+
+  [getX, getY, getZ, getW].forEach(getAxis => {
+    const [ sideA, sideB ] = getAxis(grid, row, col);
+    for (let spot of sideA) {      
+      if (spot !== '.') {
+        neighbors.push(spot);
+        break;
+      }
+    }
+
+    for (let spot of sideB) {      
+      if (spot !== '.') {
+        neighbors.push(spot);
+        break;
+      }
+    }
+  });
+
+  return neighbors;
 };
 
 const simulateRound = (grid) => {
@@ -79,19 +140,15 @@ const simulateRound = (grid) => {
     for (let col = 1; col < grid[0].length - 1; col += 1) {
       const center = grid[row][col];
 
-      const adjacencies = [
-        grid[row - 1][col] === '#',
-        grid[row + 1][col] === '#',
-        grid[row][col + 1] === '#',
-        grid[row][col - 1] === '#',
-        grid[row - 1][col + 1] === '#',
-        grid[row + 1][col + 1] === '#',
-        grid[row - 1][col - 1] === '#',
-        grid[row + 1][col - 1] === '#'
-      ];
+      const neighbors = nearestNeighbors(grid, row, col);
 
-      const numOccupied = adjacencies.reduce((a, b) => a + b, 0);
+      let numOccupied = 0;
 
+      for (let neighbor of neighbors) {
+        if (neighbor === '#')
+          numOccupied += 1;
+      }
+      
       if (center === 'L' && numOccupied === 0) {
         newGrid[row][col] = '#';
         isStable = false;
@@ -107,8 +164,6 @@ const simulateRound = (grid) => {
     newGrid,
     isStable,
   };
-}
+};
 
-
-
-solve().then(console.log); // 1848
+solve().then(console.log); // 2068
